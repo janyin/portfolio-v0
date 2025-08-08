fbb696aca6bf486e0e52591092a7f85a5be0f8da
banana <liujiangdu@gmail.com>
2025-08-08 21:09:40 +0800

    feat: Integrate Google Analytics and update dependencies for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Jiangdu | Full-Stack Web Developer | liucodes.dev',
-  description: 'liucodes.dev is the personal portfolio of Jiangdu Liu, a full stack developer specializing in TypeScript, React, and SAP UI5. Showcasing professional experience, technical projects, and a focus on scalable web application development.',
-}
+  title: "Jiangdu | Full-Stack Web Developer | liucodes.dev",
+  description:
+    "liucodes.dev is the personal portfolio of Jiangdu Liu, a full stack developer specializing in TypeScript, React, and SAP UI5. Showcasing professional experience, technical projects, and a focus on scalable web application development.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-1TMT4C27BQ" />
     </html>
-  )
+  );
 }
